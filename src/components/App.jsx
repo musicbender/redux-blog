21e2d4b02553bfedbfa6ca06e8f761893b23af9a
Pat Jacobs <musicbender@gmail.com>
2017-01-21 14:53:50 -0800

    Changed header, readme, and prepared for production

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Blog Posts</h1>
+        <h1>Pat's React Blog</h1>
         {this.props.children}
       </div>
     );
